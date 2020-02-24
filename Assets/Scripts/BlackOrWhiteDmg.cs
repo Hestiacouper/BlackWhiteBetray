e210946fc52b6aa621c5dc9584ca32ef22a19899
Kevincouper <fawderfox@gmail.com>
2020-02-24 04:18:38 +0100

    Black or white doing damage changed after X seconds

---
+++ Assets/Scripts/BlackOrWhiteDmg.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackOrWhiteDmg : MonoBehaviour
+{
+    public static bool blackDamage = true;
+    public static bool whiteDamage = false;
+    void Start()
+    {
+        InvokeRepeating("BlackOrWhiteDamage", 0.0f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BlackOrWhiteDamage()
+    {
+        blackDamage = !blackDamage;
+        whiteDamage = !whiteDamage;
+        Debug.Log("BlackDamage:");
+        Debug.Log(blackDamage);
+        Debug.Log("WhiteDamage:");
+        Debug.Log(whiteDamage);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    Rigidbody2D body;
+    Vector2 direction;
+    SpriteRenderer m_SpriteRenderer;
+    
+    
+    [SerializeField] int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void FixedUpdate() {
+        
+        body.velocity = direction;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        
+        
+    }
+
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Black") && BlackOrWhiteDmg.blackDamage || other.CompareTag("White") &&BlackOrWhiteDmg.whiteDamage)
+        {
+            TakingDamage();
+        }
+        else if(other.CompareTag("White") && !BlackOrWhiteDmg.whiteDamage || other.CompareTag("Black") && !BlackOrWhiteDmg.blackDamage)
+        {
+            NotTakingDamage();
+        }
+    }
+
+    void TakingDamage()
+    {
+        m_SpriteRenderer.color = Color.red;
+    }
+
+    void NotTakingDamage()
+    {
+        m_SpriteRenderer.color = Color.green;
+    }
+    
+    
+}
