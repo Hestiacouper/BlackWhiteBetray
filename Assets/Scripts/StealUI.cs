70fe29838eb2e8dd5ad2a07dcaa0d6eda9c8c483
Kevincouper <fawderfox@gmail.com>
2020-02-25 03:24:31 +0100

    Code to steal UI/Return UI at random time

---
+++ Assets/Scripts/StealUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("StealUICall",0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StealUICall()
+    {
+        float randomTime = Random.Range(10, 30);
+ 
+        // Steal UI code
+ 
+        Invoke("StealUICall", randomTime);
+    }
+}
